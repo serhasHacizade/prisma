e31b4f16cef901cab48ede4981a3d61759dbeed8
serhasHacizade <eren.felek.4@gmail.com>
2024-06-01 11:14:49 +0300

    commit

--- budget-tracker/app/(auth)/layout.tsx
+++ budget-tracker/app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/components/Logo";
+import { Logo } from "@/components/Logo";
 
 
 const layout = ({ children }: { children: React.ReactNode }) => {
--- budget-tracker/components/providers/RootProviders.tsx
+++ budget-tracker/components/providers/RootProviders.tsx
@@ -1,11 +1,23 @@
 "use client"
 
 import { ThemeProvider } from "next-themes";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-const RootProviders = ({children}: {children: React.ReactNode}) => {
-    return <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-        {children}
-    </ThemeProvider>;
+const RootProviders = ({ children }: { children: React.ReactNode }) => {
+
+    const [queryClient] = useState(() => new QueryClient({}));
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    )
+
 }
- 
+
 export default RootProviders;
