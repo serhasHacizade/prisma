977b42ce86f38e8302c42c9ee3c5cb33ffda0d78
serhasHacizade <eren.felek.4@gmail.com>
2024-06-11 10:31:38 +0300

    commit

---
+++ budget-tracker/app/(dashboard)/loading.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const loading = () => {
+  return (
+    null
+  )
+}
+
+export default loading
--- budget-tracker/components/CurrencyComboBox.tsx
+++ budget-tracker/components/CurrencyComboBox.tsx
@@ -8,7 +8,11 @@ import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { useMediaQuery } from "@/hooks/use-media-query"
 import { Currencies, Currency } from "@/lib/currencies"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useMutation } from "@tanstack/react-query"
+import SkeletonWrapper from "./SkeletonWrapper"
+import { UserSettings } from "@prisma/client"
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings"
+import { toast } from "sonner"
 
 
 
@@ -17,41 +21,74 @@ export function CurrencyComboBox() {
   const isDesktop = useMediaQuery("(min-width: 768px)");
   const [selectedOption, setSelectedOption] = React.useState<Currency | null>(null);
 
-  const userSettings = useQuery({
+  const userSettings = useQuery<UserSettings>({
     queryKey: ["userSettings"],
     queryFn: () => fetch("/api/user-settings").then((res) => res.json()),
   });
 
-  console.log("@@@ user settings: ", userSettings);
-  
+  React.useEffect(() => {
+    if (!userSettings.data) return;
+
+    const userCurrency = Currencies.find((currency) =>
+      currency.value === userSettings.data.currency)
+    if (userCurrency) setSelectedOption(userCurrency);
+  }, [userSettings.data]);
+
+  const mutation = useMutation({
+    mutationFn: UpdateUserCurrency,
+    onSuccess: (data: UserSettings) => {
+      toast.success(`Currency updated succesfully `, { id: "update-currency" });
+      setSelectedOption(Currencies.find((c) => c.value === data.currency )|| null)
+    },
+    onError: (e) => {
+      console.error(e);
+      toast.error("Something went wrong!", { id: "update-currency" })
+
+    }
+  });
+
+  const selectOption = React.useCallback((currency: Currency | null) => {
+    if (!currency) {
+      toast.error("Please select a currency");
+      return;
+    }
+    toast.loading("Updating currency...", { id: "update-currency" });
+    mutation.mutate(currency.value);
+  }, [mutation]);
+
   if (isDesktop) {
     return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="w-[150px] justify-start">
-            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </PopoverContent>
-      </Popover>
+      <SkeletonWrapper isLoading={userSettings.isFetching}>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="w-[150px] justify-start"
+              disabled={mutation.isPending}>
+              {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0" align="start">
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+          </PopoverContent>
+        </Popover>
+      </SkeletonWrapper>
     )
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-start">
-          {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button variant="outline" className="w-full justify-start">
+            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mt-4 border-t">
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </SkeletonWrapper>
   )
 }
 
