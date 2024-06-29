656e746e143fb5263488fcbc3fe4c1b25a1cba69
serhasHacizade <eren.felek.4@gmail.com>
2024-06-29 11:57:20 +0300

    commit

--- budget-tracker/app/(dashboard)/_components/History.tsx
+++ budget-tracker/app/(dashboard)/_components/History.tsx
@@ -1,50 +1,100 @@
-"use client"
+"use client";
 
-import { Badge } from '@/components/ui/badge';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
-import { GetFormatterForCurrency } from '@/lib/helpers';
-import { Period, Timeframe } from '@/lib/types';
-import { UserSettings } from '@prisma/client'
-import React, { useMemo, useState } from 'react'
-import HistoryPeriodSelector from './HistoryPeriodSelector';
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { GetFormatterForCurrency } from "@/lib/helpers";
+import { Period, Timeframe } from "@/lib/types";
+import { UserSettings } from "@prisma/client";
+import React, { useMemo, useState } from "react";
+import HistoryPeriodSelector from "./HistoryPeriodSelector";
+import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
 
-const History = ({userSettings}: {userSettings: UserSettings}) => {
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-    const [timeframe, setTimeframe] = useState<Timeframe>("month");
-    const [period, setPeriod] = useState<Period>({
-        month: new Date().getMonth(),
-        year: new Date().getFullYear(),
-    })
+const History = ({ userSettings }: { userSettings: UserSettings }) => {
+  const [timeframe, setTimeframe] = useState<Timeframe>("month");
+  const [period, setPeriod] = useState<Period>({
+    month: new Date().getMonth(),
+    year: new Date().getFullYear(),
+  });
 
-    const formatter = useMemo(() => {
-        return GetFormatterForCurrency(userSettings.currency);
-    }, [userSettings.currency])
+  const formatter = useMemo(() => {
+    return GetFormatterForCurrency(userSettings.currency);
+  }, [userSettings.currency]);
 
+  const historyDataQuery = useQuery({
+    queryKey: ["overview", "history", timeframe, period],
+    queryFn: () =>
+      fetch(
+        `/api/history-data?timeframe=${timeframe}&year=${period.year}&month=${period.month}`
+      ).then((res) => res.json()),
+  });
+
+  const dataAvailable =
+    historyDataQuery.data && historyDataQuery.data.length > 0;
 
   return (
     <div className="container">
-        <h2 className="mt-12 text-3xl fontbi">History</h2>        
-        <Card className="col-span-12 mt-2 w-full">
-            <CardHeader className="gap-2">
-                <CardTitle className="grid grid-flow-row justify-between gap-2 md:grid-flow-col">
-                    <HistoryPeriodSelector period={period} setPeriod={setPeriod} 
-                    timeframe={timeframe} setTimeframe={setTimeframe} />
+      <h2 className="mt-12 text-3xl fontbi">History</h2>
+      <Card className="col-span-12 mt-2 w-full">
+        <CardHeader className="gap-2">
+          <CardTitle className="grid grid-flow-row justify-between gap-2 md:grid-flow-col">
+            <HistoryPeriodSelector
+              period={period}
+              setPeriod={setPeriod}
+              timeframe={timeframe}
+              setTimeframe={setTimeframe}
+            />
 
-                    <div className="flex h-10 gap-2">
-                        <Badge variant="outline" className="flex items-center gap-2 text-sm">
-                            <div className="h-4 w-4 rounded-full bg-emerald-500"></div>
-                            Income
-                        </Badge>
-                        <Badge variant="outline" className="flex items-center gap-2 text-sm">
-                            <div className="h-4 w-4 rounded-full bg-red-500"></div>
-                            Expense
-                        </Badge>
-                    </div>
-                </CardTitle>
-            </CardHeader>
-        </Card>
+            <div className="flex h-10 gap-2">
+              <Badge
+                variant="outline"
+                className="flex items-center gap-2 text-sm"
+              >
+                <div className="h-4 w-4 rounded-full bg-emerald-500"></div>
+                Income
+              </Badge>
+              <Badge
+                variant="outline"
+                className="flex items-center gap-2 text-sm"
+              >
+                <div className="h-4 w-4 rounded-full bg-red-500"></div>
+                Expense
+              </Badge>
+            </div>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SkeletonWrapper isLoading={historyDataQuery.isFetching}>
+            {dataAvailable && (
+                <ResponsiveContainer width={"100%"} height={300}>
+                    <BarChart height={300} data={historyDataQuery.data} barCategoryGap={5}>
+                        <defs></defs>
+                    </BarChart>
+                </ResponsiveContainer>
+            )}
+            {!dataAvailable && (
+              <Card className="flex h-[300px] flex-col items-center justify-center bg-background">
+                No data for the selected period
+                <p className="text-sm text-muted-foreground">
+                  Try selecting a different period or adding new transactions
+                </p>
+              </Card>
+            )}
+          </SkeletonWrapper>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default History
+export default History;
