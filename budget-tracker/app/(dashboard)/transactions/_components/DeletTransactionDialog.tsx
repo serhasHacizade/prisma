5173e83f7e557ed494de8d40e17db802f1d70c2c
serhasHacizade <eren.felek.4@gmail.com>
2024-07-04 14:47:15 +0300

    commit

---
+++ budget-tracker/app/(dashboard)/transactions/_components/DeletTransactionDialog.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React from "react";
+import { DeleteCategory } from "../../_actions/categories";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  transactionId: string;
+}
+
+const DeletTransactionDialog = ({ open, setOpen, transactionId }: Props) => {
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation({
+    mutationFn: DeleteTransaction,
+    onSuccess: async () => {
+      toast.success("Transaction deleted successfully", {
+        id: transactionId,
+      });
+      await queryClient.invalidateQueries({
+        queryKey: ["transactions"],
+      });
+    },
+    onError: () => {
+      toast.error("Something went wrong", {
+        id: transactionId,
+      });
+    },
+  });
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            category
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading("Deleting transaction...", { id: transactionId });
+              deleteMutation.mutate(transactionId);
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeletTransactionDialog;
--- budget-tracker/app/api/transactions-history/route.ts
+++ budget-tracker/app/api/transactions-history/route.ts
@@ -1,10 +1,14 @@
+import { GetFormatterForCurrency } from "@/lib/helpers";
+import prisma from "@/lib/prisma";
 import { OverviewQuerySchema } from "@/schema/overview";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export const GET = async (request: Request) => {
-  const user = currentUser();
-  if (!user) redirect("/sign-in");
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
 
   const { searchParams } = new URL(request.url);
   const from = searchParams.get("from");
@@ -14,6 +18,7 @@ export const GET = async (request: Request) => {
     from,
     to,
   });
+
   if (!queryParams.success) {
     return Response.json(queryParams.error.message, {
       status: 400,
@@ -22,11 +27,45 @@ export const GET = async (request: Request) => {
 
   const transactions = await getTransactionsHistory(
     user.id,
-    queryParams.from,
-    queryParams.to
+    queryParams.data.from,
+    queryParams.data.to
   );
 
-  return transactions;
-};
+  return Response.json(transactions);
+}
 
-const getTransactionsHistory = async (userId: string, from: Date, to: Date) => {};
+export type GetTransactionHistoryResponseType = Awaited<
+  ReturnType<typeof getTransactionsHistory>
+>;
+
+const getTransactionsHistory = async (userId: string, from: Date, to: Date) => {
+  const userSettings = await prisma.userSettings.findUnique({
+    where: {
+      userId,
+    },
+  });
+  if (!userSettings) {
+    throw new Error("user settings not found");
+  }
+
+  const formatter = GetFormatterForCurrency(userSettings.currency);
+
+  const transactions = await prisma.transaction.findMany({
+    where: {
+      userId,
+      date: {
+        gte: from,
+        lte: to,
+      },
+    },
+    orderBy: {
+      date: "desc",
+    },
+  });
+
+  return transactions.map((transaction) => ({
+    ...transaction,
+    // lets format the amount with the user currency
+    formattedAmount: formatter.format(transaction.amount),
+  }));
+}
